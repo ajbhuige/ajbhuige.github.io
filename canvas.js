eaa44738cef3c17ca68f6b377a4e22159b8e2562
Angie <ajbhuige@outlook.com>
2019-09-10 14:33:54 +0200

    Added canvas

---
+++ canvas.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("diamond");
+var ctx = canvas.getContext('2d');
+ctx.moveTo(400,600);
+ctx.lineTo(120,350);
+ctx.lineTo(400,100);
+ctx.lineTo(680,350);
+ctx.lineTo(400,600);
+ctx.stroke();
