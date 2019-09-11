1142a1c68d1f2ea050242de3df27d1ad6608aafe
Angie <ajbhuige@outlook.com>
2019-09-11 11:41:47 +0200

    Finalize Diamond
    Added navbar and styling

--- canvas.js
+++ canvas.js
@@ -1,3 +1,5 @@
+function drawDiamond() {
+
 var canvas = document.getElementById("diamond");
 var ctx = canvas.getContext('2d');
 ctx.moveTo(400,600);
@@ -6,3 +8,7 @@ ctx.lineTo(400,100);
 ctx.lineTo(680,350);
 ctx.lineTo(400,600);
 ctx.stroke();
+}
+
+
+
